b70c6d5bf768a33c87c5dc1545fcec814d38010e
ElenaOwa <len.owa@yandex.by>
2017-11-11 15:34:12 +0300

    add api

---
+++ api/controllers/MassageController.js
@@ -0,0 +1,11 @@
+/**
+ * MassageController
+ *
+ * @description :: Server-side logic for managing massages
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
