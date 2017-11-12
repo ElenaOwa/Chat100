0507e224b40d896c3a5038b3a4513f036f729e9a
ElenaOwa <len.owa@yandex.by>
2017-11-12 11:02:43 +0300

    hash password

--- api/models/User.js
+++ api/models/User.js
@@ -4,10 +4,24 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
   attributes: {
+	username: 'string',
+    password: 'string',
+    loggedIn: 'boolean'
+    
+  },
+  
+    beforeCreate: function (values, next) {
+
+    bcrypt.hash(values.password, 8 , function (err, hash) {
+      if (err) return next(err);
+      values.password = hash;
+      next();
+    });
 
   }
 };
